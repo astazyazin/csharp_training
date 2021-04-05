972e224ce47029d3365004663824a4dd7a08f401
astazyazin <63644368+astazyazin@users.noreply.github.com>
2021-04-05 11:22:51 +0300

    Lecture 8 task 18

---
+++ addressbook_tests_autoit/tests/GroupRemovingTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    class GroupRemovingTests : TestBase
+    {
+        [Test]
+        public void TestGroupRemoval()
+        {
+            //если ни одной группы нет, то создаем ее
+     
+            if (app.Groups.GetGroupList().Count == 0)
+            {
+                app.Groups.Add(new GroupData()
+                {
+                    Name = "test22"
+                });
+            }
+
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            app.Groups.DeleteGroup(3);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+
+            oldGroups.RemoveAt(3);
+            Assert.AreEqual(oldGroups, newGroups);
+
+        }
+    }
+}
